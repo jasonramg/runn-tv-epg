be3484dcf61bb4d01eb028ad69032984858fe2eb
Jason Ram <pannuram420@gmail.com>
2026-07-06 16:22:28 +0530

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from .downloader import Downloader
+from lxml import etree
+
+from src.downloader import Downloader
 
 
 def main():
-
     xml = Downloader().download()
 
     print(f"Downloaded {len(xml)} bytes")
+
+    root = etree.fromstring(xml)
+
+    print(f"Root tag: {root.tag}")
+
+    items = root.findall("item")
+
+    print(f"Channels found: {len(items)}")
 
 
 if __name__ == "__main__":
